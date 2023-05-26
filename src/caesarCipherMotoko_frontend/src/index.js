2d5622579edfde1c73cab79a6bed66582d0cd9cc
Rajat Hooja <rajat.hooja@netleap.net>
2023-05-26 15:02:19 -0500

    added frontend

--- src/caesarCipherMotoko_frontend/src/index.js
+++ src/caesarCipherMotoko_frontend/src/index.js
@@ -1,19 +1,49 @@
 import { caesarCipherMotoko_backend } from "../../declarations/caesarCipherMotoko_backend";
 
+// declare the decode button element
+const decodeButton = document.getElementById('decode');
+
+// encode button action
 document.querySelector("form").addEventListener("submit", async (e) => {
   e.preventDefault();
   const button = e.target.querySelector("button");
 
-  const name = document.getElementById("name").value.toString();
+  // capture the form data
+  const message = document.getElementById("message").value.toString();
+  const offset = parseInt(document.getElementById("offset").value);
 
   button.setAttribute("disabled", true);
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await caesarCipherMotoko_backend.greet(name);
+  // Interact with caesarCipher actor, setting the offset and calling the encode method
+  await caesarCipherMotoko_backend.setOffset(offset);
+  const processedOutput = await caesarCipherMotoko_backend.encodeInput(message);
 
   button.removeAttribute("disabled");
 
-  document.getElementById("greeting").innerText = greeting;
+  // display the encoded message
+  document.getElementById("processedOutput").innerText = processedOutput;
+
+  return false;
+});
+
+// decode button action
+decodeButton.addEventListener('click', async (e) => {
+  e.preventDefault();
+
+  // capture the form data
+  const message = document.getElementById("message").value.toString();
+  const offset = parseInt(document.getElementById("offset").value);
+
+  decodeButton.setAttribute("disabled", true);
+
+  // Interact with caesarCipher actor, setting the offset and calling the decode method
+  await caesarCipherMotoko_backend.setOffset(offset);
+  const processedOutput = await caesarCipherMotoko_backend.decodeInput(message);
+
+  decodeButton.removeAttribute("disabled");
+
+  // display the encoded message
+  document.getElementById("processedOutput").innerText = processedOutput;
 
   return false;
 });
